98653c5c598c7a258f8181f76a4667e89775a75a
batuhanbaybas <b.baybas@gmail.com>
2023-05-23 19:36:39 +0300

    browser header helper

---
+++ src/helpers/page-headers.js
@@ -0,0 +1,5 @@
+export const pageHeader = {
+  Todo: { title: "Todo", description: "This is Todo Page" },
+  OnProgress: { title: "On Progress", description: "On Progress todo" },
+  Done: { title: "Done", description: "All Completed Todos" }
+};
