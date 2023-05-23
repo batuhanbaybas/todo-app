c8a9141aa7989ffe6b40f7d0e1580e11663d851e
batuhanbaybas <b.baybas@gmail.com>
2023-05-23 19:36:15 +0300

    helpers

---
+++ src/helpers/todo-status.js
@@ -0,0 +1,14 @@
+export const todoStatus = [
+  {
+    key: "todo",
+    value: "Todo"
+  },
+  {
+    key: "onProgress",
+    value: "On Progress"
+  },
+  {
+    key: "done",
+    value: "Done"
+  }
+];
